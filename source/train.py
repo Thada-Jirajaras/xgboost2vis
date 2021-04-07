7747abf4c649209a43c6055411db2d7c5033d343
Thada-Jirajaras <thada.jir@gmail.com>
2021-04-07 10:15:06 +0700

    Add model ability to deal with missing values from features

--- source/train.py
+++ source/train.py
@@ -23,11 +23,15 @@ def train(model,
     
     # extract rules from fitted model
     ruledf = model._Booster.trees_to_dataframe()
-    ruledf = pd.melt(ruledf, id_vars = ['ID', "Tree", "Feature", "Split"], value_vars= ["Yes", "No", "Missing"], 
+    ruledf['missing_with_yes'] = ruledf['Missing'] == ruledf['Yes']
+    ruledf['missing_with_no'] =  ruledf['Missing'] == ruledf['No']
+    ruledf = pd.melt(ruledf, id_vars = ['ID', "Tree", "Feature", "Split", "missing_with_yes", "missing_with_no"], value_vars= ["Yes", "No"], 
                 var_name = 'cat', value_name = 'to')
     ruledf['Split'] = ruledf['Split'].map(lambda x: round(x, 2))
-    ruledf['rule'] = ruledf[['Feature','Split', 'cat']].apply(lambda x: f'{x["Feature"]} < {x["Split"]}' if x['cat'] in set(['Yes', 'Missing']) 
-                                      else f'{x["Feature"]} >= {x["Split"]}', axis=1)
+    ruledf['rule'] = ruledf[['Feature','Split', 
+                             'cat', 'missing_with_yes',
+                            'missing_with_no']].apply(lambda x: f'{x["Feature"]} < {x["Split"]}' + ('or missing' if x['missing_with_yes'] else '') if x['cat'] in set(['Yes', 'Missing']) 
+                                else f'{x["Feature"]} >= {x["Split"]}'  + ('or missing' if x['missing_with_no']  else ''), axis=1).drop(columns = ['missing_with_yes', 'missing_with_no'])
     leaves = ruledf.loc[ruledf.Feature == 'Leaf', ["Tree", 'ID']].drop_duplicates().rename(columns = {'ID': 'leaf_index'})
     nodes = ruledf[['ID', 'to', 'rule']].dropna().drop_duplicates()
     def rule_for_each_leave(leaf_index, nodes):
